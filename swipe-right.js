060f50ac056bcffef42016388fe336a9508f8948
Parth Patel <parthpatel317@gmail.com>
2018-04-09 11:00:02 -0700

    write script. add setInterval next

--- swipe-right.js
+++ swipe-right.js
@@ -2,5 +2,16 @@ var swipeButton = document.querySelector('.recsGamepad__button--like');
 var likeCount = 0;
 
 function swipeRight() {
-  
+  likeCount++;
+  console.log('Swiped!' + likeCount);
+  //Can adjust the line below to increase/decrease the swiping speed
+  var timeBetweenSwipe = Math.random() * (4000 - 4000) + 2000; 
+  swipeButton.click();
+  if (likeCount < 80) {
+    setTimeout(swipeRight, timeBetweenSwipe);
+  } else {
+    console.log('take 5')
+  }
 }
+
+swipeRight();
